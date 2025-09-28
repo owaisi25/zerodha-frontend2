3ca5ec03557c95d88df31f9694ffa3f3f8f18725
Mohd Owais <198442674+owaisi25@users.noreply.github.com>
2025-09-28 17:28:21 +0530

    Update ProductPage.js

--- src/landing_page/products/ProductPage.js
+++ src/landing_page/products/ProductPage.js
@@ -1,21 +1,64 @@
 import React from "react";
 import Hero from "../products/Hero";
-import Leftsection from "./LeftSection";
+import LeftSection from "./LeftSection";
 import RightSection from "./RightSection";
 import Universe from "./Universe";
+
 function ProductPage() {
   return (
     <>
       <Hero />
-      <Leftsection imageUrl="media/images/kite.png" productName="Kite" productDescription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices." tryDemo="" learnMore="" googlePlay="" appStore="" />
-      <RightSection imageUrl="media/images/console.png" productName="Console" productDescription="A powerful reporting and analytics platform that helps you track, analyze, and optimize your investments. Access your portfolio performance and other insights on the go with our mobile app."  />
-      <Leftsection imageUrl="media/images/coin.png" productName="Coin" productDescription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices" tryDemo="" learnMore="" googlePlay="" appStore="" />
-       <RightSection imageUrl="media/images/kiteconnect.png" productName="Kite Connect API" productDescription="Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase."  />
-      <Leftsection imageUrl="media/images/varsity.png" productName="Varsity mobile" productDescription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go." tryDemo="" learnMore="" googlePlay="" appStore="" />
-   <p className="text-center"> Want to know more about our technology stack? Check out the Zerodha.tech blog.</p>
-   <Universe />
+
+      <LeftSection
+        imageUrl="/media/images/kite.png"
+        productName="Kite"
+        productDescription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+
+      <RightSection
+        imageUrl="/media/images/console.png"
+        productName="Console"
+        productDescription="A powerful reporting and analytics platform that helps you track, analyze, and optimize your investments. Access your portfolio performance and other insights on the go with our mobile app."
+      />
+
+      <LeftSection
+        imageUrl="/media/images/coin.png"
+        productName="Coin"
+        productDescription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices"
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+
+      <RightSection
+        imageUrl="/media/images/kiteconnect.png"
+        productName="Kite Connect API"
+        productDescription="Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase."
+      />
+
+      <LeftSection
+        imageUrl="/media/images/varsity.png"
+        productName="Varsity mobile"
+        productDescription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+
+      <p className="text-center">
+        Want to know more about our technology stack? Check out the Zerodha.tech blog.
+      </p>
+
+      <Universe />
     </>
   );
 }
 
 export default ProductPage;
+
