0b27cc4bf713a97952297881a6e3381db8e20970
Mohd Owais <198442674+owaisi25@users.noreply.github.com>
2025-09-28 17:24:26 +0530

    Update Hero.js

--- src/landing_page/pricing/Hero.js
+++ src/landing_page/pricing/Hero.js
@@ -11,30 +11,36 @@ function Hero() {
             Free equity investment and flat ₹20 intraday and F&amp;O trades
           </h3>
         </div>
-        <div className="row p-5 mt-5 test-center">
-            <div className="col-4 p-4">
-                <img src="media/images/pricingEquity.svg"/>
-                <h1 className="fs-3">Free equity delivery</h1>
-                <p className="text-muted">All equity delivery investments (NSE, BSE), are absolutely free — ₹ 0 brokerage.</p>
-            </div>
-            <div className="col-4 p-4">
-                 <img src="media/images/intradayTrades.svg"/>
-                <h1 className="fs-3">Intraday and F&O trades</h1>
-                <p className="text-muted">Flat ₹ 20 or 0.03% (whichever is lower) per executed order on intraday trades across equity, currency, and commodity trades. Flat ₹20 on all option trades.</p>
-            
-            </div>
-            <div className="col-4 p-4">
-                  <img src="media/images/pricingEquity.svg"/>
-                <h1 className="fs-3">Free direct MF</h1>
-                <p className="text-muted">All direct mutual fund investments are absolutely free — ₹ 0 commissions & DP charges.</p>
-            
-            </div>
+
+        <div className="row p-5 mt-5 text-center">
+          <div className="col-4 p-4">
+            <img src="/media/images/pricingEquity.svg" alt="Pricing Equity" />
+            <h1 className="fs-3">Free equity delivery</h1>
+            <p className="text-muted">
+              All equity delivery investments (NSE, BSE), are absolutely free — ₹ 0 brokerage.
+            </p>
+          </div>
+
+          <div className="col-4 p-4">
+            <img src="/media/images/intradayTrades.svg" alt="Intraday Trades" />
+            <h1 className="fs-3">Intraday and F&O trades</h1>
+            <p className="text-muted">
+              Flat ₹ 20 or 0.03% (whichever is lower) per executed order on intraday trades across equity, currency, and commodity trades. Flat ₹20 on all option trades.
+            </p>
+          </div>
+
+          <div className="col-4 p-4">
+            <img src="/media/images/pricingEquity.svg" alt="Free Direct MF" />
+            <h1 className="fs-3">Free direct MF</h1>
+            <p className="text-muted">
+              All direct mutual fund investments are absolutely free — ₹ 0 commissions & DP charges.
+            </p>
+          </div>
         </div>
       </div>
-
-      
     </div>
   );
 }
 
 export default Hero;
+
