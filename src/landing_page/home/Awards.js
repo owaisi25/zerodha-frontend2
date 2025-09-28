1e6f44d7b14b57f8edc473842438296d9885563b
Mohd Owais <198442674+owaisi25@users.noreply.github.com>
2025-09-28 17:15:34 +0530

    Update Awards.js

--- src/landing_page/home/Awards.js
+++ src/landing_page/home/Awards.js
@@ -5,12 +5,18 @@ function Awards() {
         <div className='container mt-5'>
             <div className='row'>
                 <div className='col-6 p-5'>
-                    <img src='media/images/largestBroker.svg' alt='Largest Broker' />
+                    <img 
+                        src='/media/images/largestBroker.svg' 
+                        alt='Largest Broker' 
+                    />
                 </div>
                 <div className='col-6 p-5 mt-5'>
                     <h1 style={{ fontSize: '1.75rem' }}>Largest stock broker in India</h1>
 
-                    <p className='mb-5'>2+ million Zerodha clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:</p>
+                    <p className='mb-5'>
+                        2+ million Zerodha clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:
+                    </p>
+
                     <div className='row'>
                         <div className='col-6'>
                             <ul>
@@ -21,13 +27,18 @@ function Awards() {
                         </div>
                         <div className='col-6'>
                             <ul>
-                                <li>Stoks & IPOs</li>
+                                <li>Stocks & IPOs</li>
                                 <li>Direct mutual funds</li>
                                 <li>Bonds and Govt securities</li>
                             </ul>
                         </div>
                     </div>
-                    <img src='media/images/pressLogos.png' style={{width:"90%"}}/>
+
+                    <img 
+                        src='/media/images/pressLogos.png' 
+                        style={{ width: "90%" }} 
+                        alt="Press Logos"
+                    />
                 </div>
             </div>
         </div>
@@ -35,3 +46,4 @@ function Awards() {
 }
 
 export default Awards;
+
